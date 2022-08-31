1f449cfd15752bdbfe5d9fcaec8d880da0140233
AngeloDalicandro <angelo.dalicandro1@gmail.com>
2022-08-31 15:39:31 +0200

    working

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('main_content')
+
+    <h1>I nostri fumetti</h1>
+
+    <ul>
+
+        @foreach
+
+            {{ }} - {{ }} - {{  }}
+
+        @endforeach
+
+    </ul>
+
+@endsection
